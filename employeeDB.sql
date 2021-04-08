e2f0b2a6ce309d8bd842648d1e9044b129b61101
Gerriss Gailey <gerrissgailey@gmail.com>
2021-04-08 16:29:19 -0600

    Initial DB tables created

---
+++ employeeDB.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employeeDB;
+CREATE database employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE department (
+  id INT NOT NULL,
+  department_name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL,
+  title VARCHAR(30) NULL,
+  salary DECIMAL NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
