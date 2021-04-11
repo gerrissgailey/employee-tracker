3668d1a8c20c961480a42f5a20d596f356b5f64b
Gerriss Gailey <gerrissgailey@gmail.com>
2021-04-10 18:42:26 -0600

    Added more functions to index.js

--- index.js
+++ index.js
@@ -9,20 +9,26 @@ const startingQuestion = () => {
         type: "list",
         name: "startQuestion",
         message: "What would you like to do?",
-        choices: ["Create Employee"]
+        choices: ["Create Employee", "Create Role", "Create Department", "Delete Employee"]
     }).then(answer => {
         switch(answer.startQuestion) {
             case "Create Employee":
                 createEmployee();
                 break;
-            // case "Delete Employee"
-            // add more cases here
+            case "Create Role":
+                createRole();
+                break;
+            case "Create Department":
+                createDepartment();
+                break;
+            case "Delete Employee":
+                deleteEmployee();
+                break;
             default:
                 process.exit();
         }
     })
 }
-
 
 function createEmployee () {
     prompt([
@@ -38,29 +44,73 @@ function createEmployee () {
         },
         {
             type: "list",
-            name: "",
+            name: "employeeRole",
             message: "What is the employee's role?",
-            choices: ["HR", "Janitor"]
+            choices: ["Manager", "HR", "Sales Representative", "Engineer", "Technical Support", "Machinist",]
         },
         {
             type: "input",
-            name: 
+            name: "isManager",
             message: "Is this employee a manager?"
         },
         {
             type: "input",
-            name: 
+            name: "whoIsManager",
             message: "Who is this employee's manager?"
         }
     ]).then(answers => {
         connection.query("INSERT INTO employee SET ?", {
             first_name: answers.firstName,
             last_name: answers.lastName,
-            // add more columns here for remaining questions
+            role_id: answers.employeeRole,
+            manager_id: answers.whoIsManager
         })
     })
 }
-    
-// create other functions to handle roles and department down here
+
+function createRole () {
+    prompt([
+        {
+            type: "input",
+            name: "roleName",
+            message: "What is the name of the role you would like to create?"
+        },
+        {
+            type: "input",
+            name: "roleSalary",
+            message: "What is the starting salary for this role?"
+        },
+        {
+            type: "input",
+            name: "roleDepartment",
+            message: "What department does this role fall under?"
+        }
+    ]).then(answers => {
+        connection.query("INSERT INTO role SET ?", {
+            title: answers.roleName,
+            salary: answers.roleSalary,
+            department_id: answers.roleDepartment
+        })
+    })
+}
+
+function createDepartment () {
+    prompt([
+        {
+            type: "input",
+            name: "newDepartment",
+            message: "What is the name of the department you would like to create?"
+        }
+    ]).then(answers => {
+        connection.query ("INSERT INTO department SET ?", {
+            department_name: answers.newDepartment
+        })
+    })
+}
+
+function deleteEmployee () {
+
+}
+
 
 // will need to do joins somehow...? 
